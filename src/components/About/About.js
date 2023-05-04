f23a3ec538584b2981df046c83cf7aeb358566f0
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2023-05-04 00:20:30 -0300

    About modifications

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,11 +8,11 @@ export default function About() {
   return (
     <FadeIn>
         <Container id="about">
-          <h2>About Me</h2>
+          <h3>About Me</h3>
           <p>Hi there! My name is Sofia!</p>
-          <p>I’m 22 years old from Buenos Aires, Argentina. Currently studying at UTN FRBA. In the last few years, I became more interested in web development, so I decided to study HMTL, CSS, and JavaScript by myself.</p>
+          <p>I'm a Frontend developer passionate about learning new technologies, currently studying engineering at UTN FRBA. </p>
           <p>I’m now looking for a junior dev position in a company that allows me to grow professionally.</p>
-          <h2>Skills</h2>
+          <h4>Technologies & Tools</h4>
           <Skills></Skills>
         </Container>
     </FadeIn>
--- src/components/About/Skills.js
+++ src/components/About/Skills.js
@@ -8,46 +8,40 @@ import {
   IoLogoSass,
   IoLogoNodejs,
 } from "react-icons/io";
-import { SiStyledcomponents } from "react-icons/si";
-import { FaGit, FaReact } from "react-icons/fa";
+import { SiStyledcomponents, SiTailwindcss, SiRedux, SiVite, SiExpress } from "react-icons/si";
+import { FaGithub, FaReact } from "react-icons/fa";
 
+
+export const skills = [
+  {label: "HMTL5", icon: <IoLogoHtml5 />},
+  {label: "CSS", icon: <IoLogoCss3 />},
+  {label: "Javascript", icon: <IoLogoJavascript />},
+  {label: "Saas", icon: <IoLogoSass />},
+  {label: "Styled Components", icon: <SiStyledcomponents />},
+  {label: "React", icon:<FaReact/>},
+  {label: "Tailwind", icon: <SiTailwindcss />},
+  {label: "Redux", icon: <SiRedux />},
+  {label: "Node.js", icon: <IoLogoNodejs />},
+  {label: "Express", icon: <SiExpress />},
+  {label: "Vite", icon: <SiVite />},
+  {label: "Github", icon: <FaGithub />},
+]
+         
+         
 export default function Skills() {
   return (
     <IconContext.Provider
       value={{ color: "#454545", size: "5rem", className: "skillIcon" }}
     >
       <List>
-        <Item>
-          <IoLogoHtml5 />
-          <p>HTML5</p>
-        </Item>
-        <Item>
-          <IoLogoCss3 />
-          <p>CSS3</p>
-        </Item>
-        <Item>
-          <IoLogoJavascript />
-          <p>Javascript</p>
-        </Item>
-        <Item>
-          <IoLogoSass />
-          <p>Sass</p>
-        </Item>
-        <Item>
-          <FaReact />
-          <p>React</p>
-        </Item>
-        <Item>
-          <SiStyledcomponents />
-          <p>Styled Components</p>
-        </Item>
-        <Item>
-          <FaGit /> <p>Git</p>
-        </Item>
-        <Item>
-          <IoLogoNodejs />
-          <p>Node</p>
-        </Item>
+      {skills.map(skill => {
+        return (
+          <Item>
+            {skill.icon}
+            <span>{skill.label}</span>
+          </Item>
+        );
+      })}
       </List>
     </IconContext.Provider>
   );
