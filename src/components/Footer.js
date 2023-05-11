9763d48ddca3b11e91a6413bf7d587bb9f2da838
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2023-05-10 23:43:16 -0300

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,14 @@ import styled from "styled-components";
 
 const Container = styled.footer`
   font-size: 0.6rem;
-  margin: 20px;
-
-  
+  margin-bottom: 20px;
+  margin-top: 50px;
 `;
 
 export default function Footer() {
   return (
     <Container>
-      <p>© Sosa Sofía | 2022</p>
+      <p>© Sosa Sofía | 2023</p>
     </Container>
   );
 }
